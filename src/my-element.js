a3e46e31a1cd52559a9509d27c74271f06755fba
Vladimir Sanchez <vladypuggu@gmail.com>
2024-05-27 14:03:21 -0600

    Added import './volkswagen-golf'
    Added import './volkswagen-jetta'
    
    Added import './volkswagen-passat'
    
    Added import './volkswagen-tiguan'
    
    Added import './volkswagen-touareg'
    
    Created position propertie
    
    Added position value
    
    Created Button to setPrevious in template
    
    Created Button to setNext in template
    
    Set position  in 0 to volkswagen-golf
    
    Set position  in 1 to volkswagen-jetta
    
    Set position  in 2 to volkswagen-passat
    
    Set position  in 3 to volkswagen-tijuan
    
    Set position  in 4 to volkswagen-tourage

--- src/my-element.js
+++ src/my-element.js
@@ -1,51 +1,59 @@
 import { LitElement, css, html } from 'lit'
-import litLogo from './assets/lit.svg'
-import viteLogo from '/vite.svg'
+import './volkswagen-golf'
+import './volkswagen-jetta'
+import './volkswagen-passat'
+import './volkswagen-tiguan'
+import './volkswagen-touareg'
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
 export class MyElement extends LitElement {
   static get properties() {
     return {
-      /**
-       * Copy for the read the docs hint.
-       */
-      docsHint: { type: String },
 
-      /**
-       * The number of times the button has been clicked.
-       */
-      count: { type: Number },
+
+      position: { type: Number },
     }
   }
 
   constructor() {
     super()
-    this.docsHint = 'Click on the Vite and Lit logos to learn more'
-    this.count = 0
+
+
+    this.position = 0
+  }
+
+  setPrevious(e) {
+    if (this.position === 0) {
+      this.position = 4
+    }
+    else {
+      this.position--
+    }
+  }
+
+  setNext(e) {
+    if (this.position == 4) {
+      this.position = 0
+    }
+    else {
+      this.position++
+    }
   }
 
   render() {
     return html`
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
+      <button @click="${this.setPrevious}">Anterior</button>
+      <button @click="${this.setNext}">Siguiente</button>
+      <h1>${this.position}</h1>
+      ${this.position === 0 ? html`<volkswagen-golf></volkswagen-golf>` : ''}
+      ${this.position === 1 ? html`<volkswagen-jetta></volkswagen-jetta>` : ''}
+      ${this.position === 2 ? html`<volkswagen-passat></volkswagen-passat>` : ''}
+      ${this.position === 3 ? html`<volkswagen-tiguan></volkswagen-tiguan>` : ''}
+      ${this.position === 4 ? html`<volkswagen-touareg></volkswagen-touareg>` : ''}
+     
+
       </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
+
     `
   }
 
